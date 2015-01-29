2be11acea9a8a5b28f2f6b73b6327d2f0de9cb67
Josh Erickson <josh@snoj.us>
2015-01-28 21:18:54 -0600

    initial commit
    If knockout and backbone were Ben Stone and Alison Scott.
    
    Features:
    Complete sync between KO and Backbone for ku.Model (extended BB.Model)
    ku.Collection (extended BB.Collection) in the works.
    ku.Collection displays values.
    
    Signed-off-by: Josh Erickson <josh@snoj.us>

---
+++ knockup.0.0.1.debug.js
@@ -0,0 +1,95 @@
+(function() {
+  window.ku = {};
+  ku.fromBB = function() {
+    var args =_.map(arguments, function(v) {
+      return v;
+    });
+    var t = ko.mapping.fromJS.bind(null, args)();
+    return t;  
+  };
+  ku.fromBB = ko.mapping.fromJS;
+  ku.subscribeAll = function(/*koobj, bbobj, bbopts*/) {
+    throw "A little something something for later.";
+  };
+  ku.Model = Backbone.Model.extend({
+    initialize: function() {
+      var self = this;
+      //Backbone.Model.apply(this, arguments);
+      this._ku = ku.fromBB(this.attributes);
+      this._getkoview = function(i) {
+        return self._ku;
+      };
+      this.on('change', function(uo, opts) {
+        if(opts.fromko) return;
+        var to = {};
+        _.each(uo.attributes, function(v, k) {
+          if(typeof v._ku !== 'undefined') {
+            to[k] = v._ku;
+          } else {
+            to[k] = v;
+          }
+        });
+        ku.fromBB(to, self._ku);
+        _.keys(self._ku.__ko_mapping__.mappedProperties)
+          .forEach(function(k) {
+            if(typeof self._ku[k] === 'undefined')
+              return;
+            if(typeof self._ku[k]._ku !== undefined) {
+              return;
+            }
+            if(typeof self._ku[k].subscribe !== 'function')
+              return;
+            self._ku[k].subscribe(function(nv) {
+              self.set(k, nv, {fromko: true});
+            });
+          });
+      });
+    }
+  });
+  ku.Collection = Backbone.Collection.extend({
+    model: ku.Model
+    ,initialize: function() {
+      var self = this;
+      var tarr = self.models.map(function(v) { return v._getkoview() });
+      self._ku = ko.mapping.fromJS(tarr);
+      self._getkoview = function(i) {
+        return self._ku;
+      };
+      this.on('change', function(uo, opts) {
+        if(opts.fromko) return;
+        ko.mapping.fromJS(uo.attributes, self._ku);
+        _.keys(self._ku.__ko_mapping__.mappedProperties)
+          .forEach(function(k) {
+            if(typeof self._ku[k] === 'undefined')
+              return;
+            if(typeof self._ku[k].subscribe !== 'function')
+              return;
+            console.log(k)
+            self._ku[k].subscribe(function(nv) {
+              self.set(k, nv, {fromko: true});
+            });
+          });
+      });
+
+      var remap = function(collection, opts) {
+        if(opts.fromko) return;
+        var tarr = collection.map(function(v) {
+          if(typeof v._getkoview === 'function')
+            return v._getkoview();
+          return v;
+        });
+        //ko.mapping.fromJS(tarr, self._ku);
+        ko.mapping.fromJS(tarr, self._ku);
+      };
+      self.on('reset', remap)
+      self.on('add', function(model, collection, opts) {
+        remap(collection, opts);
+      });
+    }
+  });
+  ku.View = Backbone.View.extend();
+  ku.applyBindings = function(view, element) {
+    var koview = view._getkoview();
+    ko.applyBindings(koview, element);
+  };
+})();
---
+++ test.js
@@ -0,0 +1,13 @@
+(function() {
+  $d = {};
+  var model = $d.model = new ku.Model({test1: 1234});
+  model.set('a', 11111234);
+  ku.applyBindings(model, $('#test1')[0]);
+
+  var m2 = $d.m2 = new ku.Model();
+  var coll = $d.coll = new ku.Collection()
+  coll.reset([1,2,3,4,5,6,7,9].map(function() { return {h: arguments[0]}; }));
+  //coll.reset()
+  m2.set('coll', coll)
+  ku.applyBindings(m2, $('#test2')[0]);
+})();
