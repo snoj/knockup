c7e2ca3007f940e877f6159e9658e1e7fa4b31ad
Josh Erickson <josh@snoj.us>
2015-01-28 21:42:25 -0600

    fix: BB and KO sync was broken. fix: ku.Collection now works for display again.
    Signed-off-by: Josh Erickson <josh@snoj.us>

--- knockup.0.0.1.debug.js
+++ knockup.0.0.1.debug.js
@@ -15,31 +15,33 @@
     initialize: function() {
       var self = this;
       //Backbone.Model.apply(this, arguments);
-      this._ku = ku.fromBB(this.attributes);
+      this._ku = ku.fromBB(self.attributes);
       this._getkoview = function(i) {
         return self._ku;
       };
       this.on('change', function(uo, opts) {
-        if(opts.fromko) return;
+        //if(opts.fromko) return;
         var to = {};
         _.each(uo.attributes, function(v, k) {
-          if(typeof v._ku !== 'undefined') {
-            to[k] = v._ku;
+          if(typeof v._getkoview !== 'undefined') {
+            to[k] = ko.mapping.toJS(v._getkoview());
           } else {
             to[k] = v;
           }
         });
         ku.fromBB(to, self._ku);
+
         _.keys(self._ku.__ko_mapping__.mappedProperties)
           .forEach(function(k) {
             if(typeof self._ku[k] === 'undefined')
               return;
-            if(typeof self._ku[k]._ku !== undefined) {
+            if(typeof self._ku[k]._ku != 'undefined') {
               return;
             }
             if(typeof self._ku[k].subscribe !== 'function')
               return;
             self._ku[k].subscribe(function(nv) {
+              if(opts.fromko) return;
               self.set(k, nv, {fromko: true});
             });
           });
@@ -50,22 +52,35 @@
     model: ku.Model
     ,initialize: function() {
       var self = this;
-      var tarr = self.models.map(function(v) { return v._getkoview() });
-      self._ku = ko.mapping.fromJS(tarr);
+      /*var tarr = self.models.map(function(v) { return v._getkoview() });
+      self._ku = ko.mapping.fromJS(tarr);*/
+      self._ku = ko.mapping.fromJS([]);
       self._getkoview = function(i) {
         return self._ku;
       };
       this.on('change', function(uo, opts) {
-        if(opts.fromko) return;
-        ko.mapping.fromJS(uo.attributes, self._ku);
+        //if(opts.fromko) return;
+        var to = {};
+        _.each(uo.attributes, function(v, k) {
+          if(typeof v._getkoview !== 'undefined') {
+            to[k] = ko.mapping.toJS(v._getkoview());
+          } else {
+            to[k] = v;
+          }
+        });
+        ku.fromBB(to, self._ku);
+
         _.keys(self._ku.__ko_mapping__.mappedProperties)
           .forEach(function(k) {
             if(typeof self._ku[k] === 'undefined')
               return;
+            if(typeof self._ku[k]._ku != 'undefined') {
+              return;
+            }
             if(typeof self._ku[k].subscribe !== 'function')
               return;
-            console.log(k)
             self._ku[k].subscribe(function(nv) {
+              if(opts.fromko) return;
               self.set(k, nv, {fromko: true});
             });
           });
--- test.js
+++ test.js
@@ -3,7 +3,9 @@
   var model = $d.model = new ku.Model({test1: 1234});
   model.set('a', 11111234);
   ku.applyBindings(model, $('#test1')[0]);
-
+  setTimeout(function() {
+    model._ku.test1(new Date());
+  }, 1000)
   var m2 = $d.m2 = new ku.Model();
   var coll = $d.coll = new ku.Collection()
   coll.reset([1,2,3,4,5,6,7,9].map(function() { return {h: arguments[0]}; }));
